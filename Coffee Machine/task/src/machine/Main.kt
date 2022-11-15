8e0165cad4dfbb3cd1ac46adfcfbaab48d729757
MyIDDQD <nickkosh@gmail.com>
2022-11-15 17:36:03 +0100

    Intermediate commit

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -4,43 +4,65 @@ val COFFEE_MACHINE = CoffeeMachine()
 
 fun main() {
     startDisplay()
-    val cupOfCoffee = Cup()
+    val cupOfCoffee = CupOfCoffee()
+    cupOfCoffee.howManyCups(enterQuantity())
+    println(cupsOfCoffee(cupOfCoffee))
 
-    val cups = enterQuantity()
+//    if (COFFEE_MACHINE.water < cupOfCoffee.water &&
+//        COFFEE_MACHINE.milk < cupOfCoffee.milk &&
+//        COFFEE_MACHINE.coffee < cupOfCoffee.coffee) {
+//        println("No, I can make only __0enter number of cups__ cups of coffee")
+//    } else if (COFFEE_MACHINE.water >= cupOfCoffee.water &&
+//        COFFEE_MACHINE.milk >= cupOfCoffee.milk &&
+//        COFFEE_MACHINE.coffee >= cupOfCoffee.coffee ) {
+//        print("Yes, I can make that amount of coffee")
+//
+//        if (COFFEE_MACHINE.water >= cupOfCoffee.water * 2 &&
+//            COFFEE_MACHINE.milk >= cupOfCoffee.milk  * 2 &&
+//            COFFEE_MACHINE.coffee >= cupOfCoffee.coffee * 2) {
+//            print(" (and even N more than that)")
+//        }
+//
+//    }
 
-    if (COFFEE_MACHINE.water < cupOfCoffee.water &&
-        COFFEE_MACHINE.milk < cupOfCoffee.milk &&
-        COFFEE_MACHINE.coffee < cupOfCoffee.coffee) {
-        println("No, I can make only __0enter number of cups__ cups of coffee")
-    } else if (COFFEE_MACHINE.water >= cupOfCoffee.water &&
-        COFFEE_MACHINE.milk >= cupOfCoffee.milk &&
-        COFFEE_MACHINE.coffee >= cupOfCoffee.coffee ) {
-        print("Yes, I can make that amount of coffee")
-
-        if (COFFEE_MACHINE.water >= cupOfCoffee.water * 2 &&
-            COFFEE_MACHINE.milk >= cupOfCoffee.milk  * 2 &&
-            COFFEE_MACHINE.coffee >= cupOfCoffee.coffee * 2) {
-            print(" (and even N more than that)")
-        }
-
-    }
-
-    canMakeAmountCoffee()
 //    val cupsForParty = Cup()
 //    cupsForParty.calculate(cups)
 //    display(cupsForParty.qty, cupsForParty.water, cupsForParty.milk, cupsForParty.coffee)
 }
 
-class Cup() {
-    var water = 200
+// Make super class Cup, in future can create any drinks
+open class Cup() {
+    open var water = 0
+
+    open fun howManyCups(qty: Int = 0) {
+        water *= qty
+    }
+}
+
+// Make subclass coffee cup,
+// with ingredients to coffee
+// and calculate how many ingredients needed to any quantity's cups
+class CupOfCoffee() : Cup() {
+    override var water = 200
     var milk = 50
     var coffee = 15
-
-    fun calculate(qty: Int = 1) {
+    override fun howManyCups(qty: Int) {
         water *= qty
         milk *= qty
         coffee *= qty
     }
+}
+
+// Calculate how many cups can make
+fun cupsOfCoffee(cup: CupOfCoffee): Int = if ( COFFEE_MACHINE.water < cup.water &&
+    COFFEE_MACHINE.milk < cup.milk &&
+    COFFEE_MACHINE.coffee < cup.coffee ) {
+    0
+} else {
+    val list = listOf(COFFEE_MACHINE.water / cup.water,
+        COFFEE_MACHINE.milk / cup.milk,
+        COFFEE_MACHINE.coffee / cup.coffee).sorted()
+    list[0]
 }
 
 class CoffeeMachine() {
