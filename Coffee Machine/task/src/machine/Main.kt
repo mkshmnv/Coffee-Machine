374e49ed6f37bf3709f3a77c8563265a740d3d32
MyIDDQD <nickkosh@gmail.com>
2022-11-16 22:03:50 +0100

    Done Stage 5/6: Keep track of the supplies

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,11 +1,18 @@
 package machine
 
 val COFFEE_MACHINE = CoffeeMachine()
-var MENU_CHOICE = ""
 
 fun main() {
-    display()
     menu()
+}
+
+// Create main class with default resources
+class CoffeeMachine() {
+    var water = 400
+    var milk = 540
+    var coffee = 120
+    var cups = 9
+    var money = 550
 }
 
 // Make super class Cup, in future can create any drinks
@@ -15,12 +22,9 @@ open class CupOfCoffee() {
     open var coffee = 0
     open var cost = 0
     var cup = 1
-    open fun howManyCups(qty: Int = 0) {
-        water *= qty
-    }
 }
 
-// Make subclass tape of coffee, with ingredients to coffee
+// Make a few subclasses tape of coffee, with ingredients to coffee
 class Espresso() : CupOfCoffee() {
     override var water = 250
     override var milk = 0
@@ -42,15 +46,8 @@ class Cappuccino() : CupOfCoffee() {
     override var cost = 6
 }
 
-class CoffeeMachine() {
-    var water = 400
-    var milk = 540
-    var coffee = 120
-    var cups = 9
-    var money = 550
-}
-
-fun display() {
+// Output resources in coffee machine
+fun remaining() {
     println("The coffee machine has:\n" +
             "${COFFEE_MACHINE.water} ml of water\n" +
             "${COFFEE_MACHINE.milk} ml of milk\n" +
@@ -59,54 +56,92 @@ fun display() {
             "$${COFFEE_MACHINE.money} of money"
     )
 }
-// Implementation request menu
+
+// Menu coffee machine
 fun menu() {
-    println("\nWrite action (buy, fill, take):")
-    MENU_CHOICE = readln()
-    when (MENU_CHOICE.toString()) {
+    println("Write action (buy, fill, take, remaining, exit):")
+    // This var need for implementation possibility of back
+    var action = readln()
+
+    when (action) {
         // Buy a coffee
-        "buy" -> buy()
-        // Load the coffee machine
-        "fill" -> fill()
+        "buy" -> {
+            println("")
+            buy()
+            println("")
+            menu()
+        }
+        // Load resources the coffee machine
+        "fill" -> {
+            println("")
+            fill()
+            println("")
+            menu()
+        }
         // Pick up money
-        "take" -> take()
+        "take" -> {
+            take()
+            menu()
+        }
+        // Pick up money
+        "remaining" -> {
+            println("")
+            remaining()
+            println("")
+            menu()
+        }
+        // Pick up money
+        "exit" -> action = ""
     }
-    display()
 }
 
-// Function of sell coffee, balance calculation and update balances
+// Function of checking resources in coffee machine
+// sell coffee
+// balance calculation
+// update balances
 fun buy() {
-    println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-    when (readln().toInt()) {
-        1 -> {
+    println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
+// Checking resources in coffee machine
+    fun checkResources(cup: CupOfCoffee) {
+        if (
+            0 > COFFEE_MACHINE.water - cup.water ||
+            0 > COFFEE_MACHINE.milk - cup.milk ||
+            0 > COFFEE_MACHINE.coffee - cup.coffee ||
+            0 > COFFEE_MACHINE.cups - cup.cup
+        ) {
+            if (0 > COFFEE_MACHINE.water - cup.water) println("Sorry, not enough water!")
+            if (0 > COFFEE_MACHINE.milk - cup.milk) println("Sorry, not enough milk!")
+            if (0 > COFFEE_MACHINE.coffee - cup.coffee) println("Sorry, not enough coffee!")
+            if (0 > COFFEE_MACHINE.cups - cup.cup) println("Sorry, not enough cups!")
+        } else {
+            println("I have enough resources, making you a coffee!")
+            COFFEE_MACHINE.water -= cup.water
+            COFFEE_MACHINE.milk -= cup.milk
+            COFFEE_MACHINE.coffee -= cup.coffee
+            COFFEE_MACHINE.cups -= cup.cup
+            COFFEE_MACHINE.money += cup.cost
+        }
+    }
+// Submenu for buy a coffee
+    when (readln()) {
+        "1" -> {
             val cupOfEspresso = Espresso()
-            COFFEE_MACHINE.water -= cupOfEspresso.water
-            COFFEE_MACHINE.milk -= cupOfEspresso.milk
-            COFFEE_MACHINE.coffee -= cupOfEspresso.coffee
-            COFFEE_MACHINE.cups -= cupOfEspresso.cup
-            COFFEE_MACHINE.money += cupOfEspresso.cost
+            checkResources(cupOfEspresso)
         }
-        2 -> {
+        "2" -> {
             val cupOfLatte = Latte()
-            COFFEE_MACHINE.water -= cupOfLatte.water
-            COFFEE_MACHINE.milk -= cupOfLatte.milk
-            COFFEE_MACHINE.coffee -= cupOfLatte.coffee
-            COFFEE_MACHINE.cups -= cupOfLatte.cup
-            COFFEE_MACHINE.money += cupOfLatte.cost
+            checkResources(cupOfLatte)
         }
-        3 -> {
+        "3" -> {
             val cupOfCappuccino = Cappuccino()
-            COFFEE_MACHINE.water -= cupOfCappuccino.water
-            COFFEE_MACHINE.milk -= cupOfCappuccino.milk
-            COFFEE_MACHINE.coffee -= cupOfCappuccino.coffee
-            COFFEE_MACHINE.cups -= cupOfCappuccino.cup
-            COFFEE_MACHINE.money += cupOfCappuccino.cost
+            checkResources(cupOfCappuccino)
         }
+        "back" -> ""
     }
     println("")
 }
 
-// Function of
+// Function of upload resources in coffee machine
 fun fill() {
     println("Write how many ml of water you want to add:")
     COFFEE_MACHINE.water += readln().toInt()
@@ -121,11 +156,13 @@ fun fill() {
     COFFEE_MACHINE.cups += readln().toInt()
 }
 
-// Function collection of money and update balances
+// Function encashment of money and update balances
 fun take() {
     println("I gave you $${COFFEE_MACHINE.money}\n")
     COFFEE_MACHINE.money = 0
 }
+
+
 
 
 
